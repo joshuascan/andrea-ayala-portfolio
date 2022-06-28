55db1b7ef49a3c21b4e80d57027366f83748f049
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-06-27 17:20:35 -0700

    production art gallery WIP

--- src/pages/production-art.js
+++ src/pages/production-art.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+import Image from "next/image";
 import Layout from "../components/Layout";
 import {
   Section,
@@ -5,6 +7,27 @@ import {
   SectionText,
   CopyrightText,
 } from "../styles/GlobalComponents";
+import { productionArt } from "../constants";
+
+const ArtContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 1px solid red;
+  width: 125rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 43rem;
+  height: 22rem;
+  position: relative;
+  border: 1px solid red;
+`;
+
+const Img = styled(Image)`
+  object-fit: contain;
+  overflow: hidden;
+`;
 
 export default function ProductionArt() {
   return (
@@ -21,6 +44,20 @@ export default function ProductionArt() {
         <CopyrightText>
           All artwork © All American Label & Packaging
         </CopyrightText>
+        <ArtContainer>
+          {productionArt.map(({ title, description, image, id }) => {
+            return (
+              <ImageContainer key={id}>
+                <Img
+                  src={image}
+                  alt={title}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </ImageContainer>
+            );
+          })}
+        </ArtContainer>
       </Section>
     </Layout>
   );
