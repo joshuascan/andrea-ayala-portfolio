235af9b7191b68885e1d19e1fd84cfef0d5cf72b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-07-08 11:03:12 -0700

    creates siteMetadata file

---
+++ src/data/siteMetadata.js
@@ -0,0 +1,9 @@
+const siteMetadata = {
+  siteName: "Andrea Wilson Design",
+  phoneNumber: "(925) 495-9221",
+  siteUrl: "https://andrea-wilson-portfolio.vercel.app/",
+  email: "aandrea714@gmail.com",
+  linkedin: "https://www.linkedin.com/in/andrea-wilson7/",
+};
+
+export default siteMetadata;
